47e870a1d533f32aef57320fe2f76bad6f934ca8
саид давудов <13isa.davudov.top@gmail.com>
2025-10-01 14:06:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ptobnaay_rabota/OknoSosActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ptobnaay_rabota
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OknoSosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.okno_sos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
